624f9b7843d5fafcb0c7afcb13cfb261e53161d3
Deepak Singh Rajput C <158419024+DeepakSinghRajputC@users.noreply.github.com>
2025-07-15 19:21:44 +0530

    Update _document.tsx

--- packages/blog-starter-kit/themes/enterprise/pages/_document.tsx
+++ packages/blog-starter-kit/themes/enterprise/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
 		<Html lang="en">
 			<Head />
 			<body>
+        <script type='text/javascript' src='//pl27172667.profitableratecpm.com/9b/5d/07/9b5d07c3a41f5910dae525fec5997c75.js'></script>
 				<Main />
 				<NextScript />
 			</body>

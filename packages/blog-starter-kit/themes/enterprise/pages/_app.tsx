e7aaf419b4047b2433b77a8c2250cfb0b37c3972
Deepak Singh Rajput C <158419024+DeepakSinghRajputC@users.noreply.github.com>
2025-07-15 20:25:34 +0530

    Update _app.tsx

--- packages/blog-starter-kit/themes/enterprise/pages/_app.tsx
+++ packages/blog-starter-kit/themes/enterprise/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import { useEffect } from 'react';
 import '../styles/index.css';
+import Script from 'next/script'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	useEffect(() => {
@@ -11,5 +12,26 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 			i.style.height = `${parseInt(newHeight)}px`;
 		};
 	}, []);
-	return <Component {...pageProps} />;
+	 return (
+    <>
+      {/* ── 1) External Adsterra script ── */}
+      <Script
+        // TODO: replace this src with your Adsterra URL
+		    async
+        src="//pl27172667.profitableratecpm.com/9b/5d/07/9b5d07c3a41f5910dae525fec5997c75.js"
+        strategy="afterInteractive"
+      />
+
+      {/* ── 2) Inline init (if needed) ── */} 
+      {/* <Script id="adsterra-init" strategy="afterInteractive">
+        {`
+          // TODO: paste your Adsterra inline snippet here, e.g.:
+          // window.adsterra = window.adsterra || [];
+          // adsterra.push({ ... });
+        `}
+      </Script> */}
+
+      {/* ── 3) Your app's main content ── */}
+      <Component {...pageProps} />
+    </>
 }
